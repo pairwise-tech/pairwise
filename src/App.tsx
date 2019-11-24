da0a621f4e09b796b312e604f5c174356fe27c81
Sean Smith <sean.smith.2009@gmail.com>
2019-11-24 17:52:26 +0800

    Only render preview every 2 seconds

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,91 @@
 import React from "react";
 import styled from "styled-components";
-import Editor from "@monaco-editor/react";
+import { ControlledEditor } from "@monaco-editor/react";
 
 import * as Babel from "@babel/standalone";
+
+/** ===========================================================================
+ * Types & Config
+ * ============================================================================
+ */
+
+interface IState {
+  code: string;
+  updatedQueued: boolean;
+}
+
+/**
+ * TODO: Imports need to be enabled but somehow not indicate "module not found"
+ * errors. Import statements then need to be dynamically parsed when the
+ * code string is executed, and use to define the dependencies which need
+ * to be fetched to run the code.
+ */
+const DEFAULT_CODE = `
+  // import React from "react";
+  // import ReactDOM from "react-dom";
+
+  class App extends React.Component {
+    render(): JSX.Element {
+      const text: string = "Hi yay!!!";
+      return (
+        <div>
+          <h1>{text}</h1>
+        </div>
+      );
+    }
+  }
+  ReactDOM.render(<App />, document.getElementById('root'));
+`;
 
 /** ===========================================================================
  * Component
  * ============================================================================
  */
 
-class App extends React.Component {
+class App extends React.Component<{}, IState> {
   iFrame: Nullable<HTMLIFrameElement> = null;
+  editorRef: any = null;
+  timeout: any = null; /* Blah, remove later when dependencies are cached */
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      code: DEFAULT_CODE,
+      updatedQueued: false,
+    };
+  }
+
+  handleEditorDidMount = (_: any, editor: any) => {
+    this.editorRef = editor;
+  };
 
   componentDidMount() {
-    const CODE = `
-      class App extends React.Component {
-        render(): JSX.Element {
-          const text: boolean = "Hi yay!!!";
-          return (
-            <div>
-              <h1>{text}</h1>
-            </div>
-          );
-        }
-      }
-      ReactDOM.render(<App />, document.getElementById('root'));
-    `;
+    this.iFrameRender();
+  }
 
-    const output = Babel.transform(CODE, {
-      presets: [
-        "es2015",
-        "react",
-        ["typescript", { isTSX: true, allExtensions: true }],
-      ],
-    }).code;
+  componentWillUnmount() {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+  }
 
+  iFrameRender = () => {
+    /**
+     * TODO: Script files should be determined dynamically from the code string
+     * `import` statements, and then fetched from UNPKG and cached. The
+     * iframe should only fetch these dependencies once and not on every
+     * render like it does not.
+     */
     const HTML_DOCUMENT = `
       <html>
-        <head />
-        <body>
-          <div id="root" />
+        <head>
           <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
           <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-          <script>${output}</script>
+        </head>
+        <body>
+          <div id="root" />
+          <script id="code">${this.transformCode()}</script>
         </body>
       </html>
     `;
@@ -50,18 +93,51 @@ class App extends React.Component {
     if (this.iFrame) {
       this.iFrame.srcdoc = HTML_DOCUMENT;
     }
-  }
+
+    this.setState({ updatedQueued: false });
+  };
+
+  transformCode = () => {
+    const output = Babel.transform(this.state.code, {
+      presets: [
+        "es2015",
+        "react",
+        ["typescript", { isTSX: true, allExtensions: true }],
+      ],
+    }).code;
+    return output;
+  };
 
   render() {
     return (
       <Page>
         <EditorContainer>
-          <Editor theme="dark" height="100vh" language="javascript" />
+          <ControlledEditor
+            theme="dark"
+            height="100vh"
+            language="typescript"
+            value={this.state.code}
+            onChange={this.handleChange}
+            editorDidMount={this.handleEditorDidMount}
+          />
         </EditorContainer>
-        <Frame ref={this.setRef} title="code-preview" />
+        <Frame id="iframe" ref={this.setRef} title="code-preview" />
       </Page>
     );
   }
+
+  handleChange = (e: any, value: string | undefined) => {
+    this.onChange(value || this.state.code);
+  };
+
+  onChange = async (value: string) => {
+    const { updatedQueued } = this.state;
+    this.setState({ code: value, updatedQueued: true }, () => {
+      if (!updatedQueued) {
+        this.timeout = setTimeout(this.iFrameRender, 2000);
+      }
+    });
+  };
 
   setRef = (r: HTMLIFrameElement) => {
     this.iFrame = r;
