d9e5523e795cfb63a8992712a5c2cbe9702ffa70
Sean Smith <sean.smith.2009@gmail.com>
2019-12-06 12:41:55 +0800

    Update todo list

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -41,26 +41,38 @@ import SyntaxHighlightWorker from "workerize-loader!../tools/tsx-syntax-highligh
  * - TODO: Things not done yet for the challenge workspace:
  *
  * HARD:
- * [~] Type definition files: find a way to use actual type definition files...
- * [~] Fetch import modules dynamically: need to find UNPKG links dynamically...
- * [x] TSX syntax highlighting
- * [x] TSX syntax support in monaco editor
- * [x] Ability to test React challenges
- * [!] Ability to run React Native challenges (react-native-web?)
- * [!] Ability to run NodeJS challenges (e.g. fs, express, etc.)
- * [!] Ability to run database challenges, (e.g. SQL, Mongo, etc.)
+ * [~] Type definition files: see codesandbox source code which dynamically
+ *     fetches and inserts @types/ dependencies for modules.
+ * [~] Fetch import modules dynamically: unknown ~
+ * [!] Ability to run React Native challenges: should be possible by importing
+ *     react-native-web and creating a "mobile-like" preview view
+ * [!] Ability to run NodeJS challenges (e.g. fs, express, etc.): may be
+ *     possible by mocking the specific functions we want to provide
+ *     testing for.
+ * [!] Ability to run database challenges, (e.g. SQL, Mongo, etc.): may
+ *     require a fixed backend environment to run database queries against,
+ *     the queries could be run in atomic transactions which are always
+ *     rolled back to preserve a consistent database state.
  * [!] Secure iframe environment from infinite loops and other unsafe code, e.g.
- *     remove alert, confirm, and other global functions from the user's code.
+ *     remove alert, confirm, and other global functions from the user's code,
+ *     securing from infinite loops may require using a web worker to execute
+ *     the code.
  *
  * EASIER:
  * [ ] Test async code challenges
  * [ ] Markdown support in challenge content and test results
+ * [ ] Improve theme styling for code editor (multi theme support?)
  * [ ] cmd+enter should run code but not enter a new line in the editor
  * [ ] Syntax/compilation errors should be reported to the workspace console
- * [ ] Improve theme styling for code editor (multi theme support?)
+ * [ ] Challenge and tests content should scroll if overflowing the resizable
+ *     container grid they are in.
+ *
+ * DONE:
+ * [x] TSX syntax highlighting
+ * [x] TSX syntax support in monaco editor
+ * [x] Ability to test React challenges
  * [x] Include console warn and info in console method overrides
  * [x] Don't show test console output in workspace console window
- *
  * ============================================================================
  */
 
