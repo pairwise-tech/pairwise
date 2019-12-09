78d4a910f7db56d6b1ad1ba452b6b74d7c114e42
Sean Smith <sean.smith.2009@gmail.com>
2019-12-09 23:59:36 +0800

    App.tsx with challenges and move todo list to file

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,49 @@ import React from "react";
 
 import Workspace from "./components/Workspace";
 
+import challenges from "./challenges/01_programming_fundamental.json";
+
+console.log(challenges);
+
+/** ===========================================================================
+ * Types & Config
+ * ============================================================================
+ */
+
+interface IState {
+  selectedChallengeUuid: Nullable<string>;
+}
+
+export interface Challenge {
+  uuid: string;
+  title: string;
+  content: string;
+  starterCode: string;
+  solutionCode: string;
+  testCode: string;
+}
+
 /** ===========================================================================
  * App
  * ============================================================================
  */
 
-class App extends React.Component {
+class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      selectedChallengeUuid: "",
+    };
+  }
+
   render(): JSX.Element {
     return <Workspace />;
   }
+
+  selectedChallengeById = (id: string) => {
+    // implement
+  };
 }
 
 /** ===========================================================================
