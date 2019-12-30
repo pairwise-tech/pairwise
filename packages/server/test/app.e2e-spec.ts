67df8ade3a16c02568c97b761f8008c0bd11cdc5
Sean Smith <sean.smith.2009@gmail.com>
2019-12-30 22:57:01 +0800

    Modify e2e test file to run against the real app

--- packages/server/test/app.e2e-spec.ts
+++ packages/server/test/app.e2e-spec.ts
@@ -1,22 +1,8 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
-import * as request from "supertest";
-import { AppModule } from "./../src/app.module";
+import request from "supertest";
 
 describe("AppController (e2e)", () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
   it("/ (GET)", () => {
-    return request(app.getHttpServer())
+    return request("http://localhost:9000")
       .get("/")
       .expect(200)
       .expect("Hello World!");
