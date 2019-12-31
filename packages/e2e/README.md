d3c58dd87db41e0dda9a20a022d58f57576fe581
Sean Smith <sean.smith.2009@gmail.com>
2019-12-31 11:53:34 +0800

    Create new e2e package and move e2e test suite there

---
+++ packages/e2e/README.md
@@ -0,0 +1,11 @@
+# `e2e`
+
+> TODO: description
+
+## Usage
+
+```
+const e2E = require('e2e');
+
+// TODO: DEMONSTRATE API
+```
--- packages/e2e/src/app.e2e-spec.ts
+++ packages/e2e/src/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 
 describe("AppController (e2e)", () => {
   it("/ (GET)", () => {
-    return request("http://localhost:9000")
+    return request("http://server:9000")
       .get("/")
       .expect(200)
       .expect("Hello World!");
