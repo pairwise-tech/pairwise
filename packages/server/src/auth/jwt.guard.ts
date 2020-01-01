7b9faec8aacf9db6e6b209685ae0bcab5df33b2c
Sean Smith <sean.smith.2009@gmail.com>
2020-01-02 03:56:53 +0800

    Comments

--- packages/server/src/auth/jwt.guard.ts
+++ packages/server/src/auth/jwt.guard.ts
@@ -5,6 +5,9 @@ import { AuthService } from "./auth.service";
  * A custom Guard which requires optional authentication. The guard
  * will simply add the user to a request if they are authenticated,
  * but unauthenticated requests will also be passed through.
+ *
+ * See more here:
+ * https://stackoverflow.com/questions/53426069/getting-user-data-by-using-guards-roles-jwt
  */
 export class CustomJwtAuthGuard extends AuthGuard("jwt") {
   constructor(private readonly authService: AuthService) {
