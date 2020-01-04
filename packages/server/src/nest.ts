c1cd482cec629049452c9e84793beaea40085d34
Sean Smith <sean.smith.2009@gmail.com>
2020-01-04 23:19:24 +0800

    Renaming, comments

--- packages/server/src/nest.ts
+++ packages/server/src/nest.ts
@@ -18,7 +18,7 @@ const swaggerOptions = new DocumentBuilder()
   .setTitle("Mono Prototype")
   .setDescription("Mono Prototype APIs")
   .setVersion("1.0")
-  .addTag("Learn to Code! 🥳")
+  .addTag("Learn to Code!")
   .build();
 
 /** ===========================================================================
@@ -29,14 +29,19 @@ const swaggerOptions = new DocumentBuilder()
 const main = async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.useGlobalPipes(new ValidationPipe());
-
+  /* Enable logging */
   app.use(morgan("dev"));
 
+  /* Enable validation pipes */
+  app.useGlobalPipes(new ValidationPipe());
+
+  /* Enable Swagger documentation */
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup("api", app, document);
 
+  /* Start the app */
   await app.listen(PORT);
+
   console.log(`\n- NestJS app launched on:    http://localhost:${PORT}/`);
   console.log(`- View Swagger API docs:     http://localhost:${PORT}/api\n`);
   console.log(`Have fun!\n`);
