84e269799c7bbc6d6f897b2240f51e6caf96d466
Sean Smith <sean.smith.2009@gmail.com>
2020-01-07 19:27:17 +0800

    Refactor Cypress auth test - awesome code

--- packages/cypress/src/integration/auth.spec.ts
+++ packages/cypress/src/integration/auth.spec.ts
@@ -1,69 +1,50 @@
 import { CLIENT_APP_URL } from "../support/utils";
 
 describe("Authentication Flows: signin, profile, and logout", () => {
-  it("Facebook Authentication", () => {
+  beforeEach(() => {
     cy.visit(CLIENT_APP_URL);
     cy.wait(2500);
-
     cy.get("#login-signup-button").click({ force: true });
-    cy.get("#facebook-login").click({ force: true });
+  });
 
-    cy.wait(2500);
-    cy.contains("Welcome, ");
-
-    cy.get("#account-menu-dropdown").trigger("mouseover");
-    cy.get("#profile-link").click({ force: true });
-    cy.contains("User Profile:");
-
-    cy.get("#account-menu-dropdown").trigger("mouseover");
-    cy.get("#logout-link").click({ force: true });
-    cy.contains("Pairwise Home");
-
-    cy.contains("Login or Signup");
-    cy.url().should("include", "home");
+  it("Facebook Authentication", () => {
+    loginByLinkId("#facebook-login");
+    assertAuthenticatedFlowWorks();
   });
 
   it("GitHub Authentication", () => {
-    cy.visit(CLIENT_APP_URL);
-    cy.wait(2500);
-
-    cy.get("#login-signup-button").click({ force: true });
-    cy.get("#github-login").click({ force: true });
-
-    cy.wait(2500);
-    cy.contains("Welcome, ");
-
-    cy.get("#account-menu-dropdown").trigger("mouseover");
-    cy.get("#profile-link").click({ force: true });
-    cy.contains("User Profile:");
-
-    cy.get("#account-menu-dropdown").trigger("mouseover");
-    cy.get("#logout-link").click({ force: true });
-    cy.contains("Pairwise Home");
-
-    cy.contains("Login or Signup");
-    cy.url().should("include", "home");
+    loginByLinkId("#github-login");
+    assertAuthenticatedFlowWorks();
   });
 
   it("Google Authentication", () => {
-    cy.visit(CLIENT_APP_URL);
-    cy.wait(2500);
-
-    cy.get("#login-signup-button").click({ force: true });
-    cy.get("#google-login").click({ force: true });
-
-    cy.wait(2500);
-    cy.contains("Welcome, ");
-
-    cy.get("#account-menu-dropdown").trigger("mouseover");
-    cy.get("#profile-link").click({ force: true });
-    cy.contains("User Profile:");
-
-    cy.get("#account-menu-dropdown").trigger("mouseover");
-    cy.get("#logout-link").click({ force: true });
-    cy.contains("Pairwise Home");
-
-    cy.contains("Login or Signup");
-    cy.url().should("include", "home");
+    loginByLinkId("#google-login");
+    assertAuthenticatedFlowWorks();
   });
 });
+
+/**
+ * Helper to test the authentication features work after login occurs.
+ */
+const assertAuthenticatedFlowWorks = () => {
+  cy.wait(2500);
+  cy.contains("Welcome, ");
+
+  cy.get("#account-menu-dropdown").trigger("mouseover");
+  cy.get("#profile-link").click({ force: true });
+  cy.contains("User Profile:");
+
+  cy.get("#account-menu-dropdown").trigger("mouseover");
+  cy.get("#logout-link").click({ force: true });
+  cy.contains("Pairwise Home");
+
+  cy.contains("Login or Signup");
+  cy.url().should("include", "home");
+};
+
+/**
+ * Helper to click the appropriate SSO login link.
+ */
+const loginByLinkId = (id: string) => {
+  cy.get(id).click({ force: true });
+};
