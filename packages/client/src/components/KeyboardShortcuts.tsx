55530b92def702ca3e4f64b3f13de9959d869caf
Ian Sinnott <ian@iansinnott.com>
2020-01-09 22:28:40 +0800

    Improve keyboard shortcut handler

--- packages/client/src/components/KeyboardShortcuts.tsx
+++ packages/client/src/components/KeyboardShortcuts.tsx
@@ -1,5 +1,7 @@
 import allPass from "ramda/es/allPass";
 import React from "react";
+import tap from "ramda/es/tap";
+import difference from "ramda/es/difference";
 
 const debug = require("debug")("client:KeyboardShortcuts");
 
@@ -15,9 +17,7 @@ const KeyboardShortcuts = ({ keymap }: KeyboardShortcutsProps) => {
       shift: "shiftKey",
       ctrl: "ctrlKey",
       alt: "altKey",
-      option: "altKey",
       cmd: "metaKey",
-      command: "metaKey",
     };
 
     const makePredicate = (commandString: string) => {
@@ -36,12 +36,38 @@ const KeyboardShortcuts = ({ keymap }: KeyboardShortcutsProps) => {
 
       const charKey = chars[chars.length - 1]; // Last
       const charPred = (e: KeyboardEvent) => e.key.toLowerCase() === charKey;
+
+      // All meta keys
       const meta = chars
         .slice(0, -1)
+        .map(
+          tap(x => {
+            // Just some validation
+            if (x === "option") {
+              throw new Error(
+                "[Err KeyboardShortcuts] 'option' key passed, expected 'alt'.",
+              );
+            }
+            if (x === "command") {
+              throw new Error(
+                "[Err KeyboardShortcuts] 'command' key passed, expected 'cmd'.",
+              );
+            }
+          }),
+        )
         .map(x => metaMap[x])
         .filter(Boolean);
+      // All meta keys not included in the sequence
+      const inverseMeta = difference(Object.values(metaMap), meta);
+
+      // Ensure all meta keys are pressed...
       const metaPred = meta.map((k: MetaKey) => (e: KeyboardEvent) => e[k]);
-      return allPass([...metaPred, charPred]);
+      // ... but NONE of the other meta keys are pressed. Example: 'cmd+shift+e' should NOT trigger 'cmd+e'
+      const inverseMetaPred = inverseMeta.map(
+        (k: MetaKey) => (e: KeyboardEvent) => !e[k],
+      );
+
+      return allPass([...metaPred, ...inverseMetaPred, charPred]);
     };
 
     const listeners = Object.keys(keymap).reduce(
