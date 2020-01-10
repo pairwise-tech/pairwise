9fbddf305b20dd3b4e72972cf0e64a051e2b7763
Sean Smith <sean.smith.2009@gmail.com>
2020-01-10 17:32:38 +0800

    Add feedback entity import to feedback module

--- packages/server/src/feedback/feedback.module.ts
+++ packages/server/src/feedback/feedback.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { FeedbackController } from "./feedback.controller";
 import { FeedbackService } from "./feedback.service";
+import { Feedback } from "./feedback.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "src/user/user.module";
 
 @Module({
   controllers: [FeedbackController],
   providers: [FeedbackService],
+  imports: [TypeOrmModule.forFeature([Feedback]), UsersModule],
 })
 export class FeedbackModule {}
