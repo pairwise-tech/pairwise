3c3e8eea540d05dbab239c326f8eeff79d6e4d90
Sean Smith <sean.smith.2009@gmail.com>
2020-01-16 23:02:14 +0800

    Fix browser-test-lib import filename

--- packages/client/src/tools/browser-test-lib.ts
+++ packages/client/src/tools/browser-test-lib.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import EXPECTATION_LIB from "!raw-loader!./browser-test-lib";
+import EXPECTATION_LIB from "!raw-loader!./browser-test-lib-source";
 
 /**
  * Isolate this module to isolate the raw-loader from being pulled into
