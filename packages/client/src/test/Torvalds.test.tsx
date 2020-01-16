79164716067bb78ceeea95449e3c1680f170fe72
Sean Smith <sean.smith.2009@gmail.com>
2020-01-16 13:34:38 +0800

    Create unit test file for testing all challenges

---
+++ packages/client/src/test/Torvalds.test.tsx
@@ -0,0 +1,5 @@
+describe("Linus should be able to pass all the challenges first try", () => {
+  test("Execute all challenge tests against their solutions", () => {
+    expect(true).toBeTruthy();
+  });
+});
