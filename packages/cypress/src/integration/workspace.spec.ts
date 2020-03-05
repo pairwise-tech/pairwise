6553ba46c94b47beba21a600486460226355c898
Ian Sinnott <ian@iansinnott.com>
2020-03-05 17:18:17 +0800

    Fix failing spec. No longer looking for a colon

--- packages/cypress/src/integration/workspace.spec.ts
+++ packages/cypress/src/integration/workspace.spec.ts
@@ -17,7 +17,7 @@ describe("Workspace and Challenge Navigation Works", () => {
     cy.visit(`${CLIENT_APP_URL}/home`);
     cy.url().should("include", "home");
     cy.contains("Welcome to Pairwise!");
-    cy.contains("Courses:");
+    cy.contains("Courses");
     cy.get("#course-link-0").contains("Fullstack TypeScript");
     cy.get("#course-link-0-start").click({ force: true });
     cy.url().should("include", "workspace");
