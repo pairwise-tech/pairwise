bf32754269b51bc1707b8f29e0c08518e629fc7a
Sean Smith <sean.smith.2009@gmail.com>
2020-03-06 11:54:09 +0800

    Finalize admin backdoor setup, add comments

--- admin.js
+++ admin.js
@@ -1,5 +1,13 @@
 const axios = require("axios");
 
+/**
+ * Admin API scripting lifestyles!
+ *
+ * This needs to be fully removed once a proper admin UI exists. This is
+ * a temporary measure to allow us to conduct admin API actions using a
+ * hard-coded access token in the production server.
+ */
+
 const SERVER_URL = process.env.SERVER_URL || "http://localhost:9000";
 const ADMIN_TOKEN = process.env.ADMIN_TOKEN || "local_admin_token";
 
--- packages/server/src/auth/admin.guard.ts
+++ packages/server/src/auth/admin.guard.ts
@@ -1,14 +1,23 @@
 import { AuthGuard } from "@nestjs/passport";
 import { UnauthorizedException, BadRequestException } from "@nestjs/common";
-import getenv = require("getenv");
+import getenv from "getenv";
+import { UserProfile } from "@pairwise/common";
 
 /**
  * Hard-coded list of administrators emails.
  */
-const WHITELISTED_ADMIN_EMAILS = new Set(["sean.smith.2009@gmail.com"]);
+const WHITELISTED_ADMIN_EMAILS = getenv.array(
+  "WHITELISTED_ADMIN_EMAILS",
+  "string",
+  [],
+);
+
+const ADMIN_EMAILS = new Set(WHITELISTED_ADMIN_EMAILS);
 
 /**
- * Hard-coded Admin Access Token. This is dangerous!!!
+ * Hard-coded Admin Access Token. This is dangerous!!! This should be fully
+ * removed in the future once a proper admin UI exists. This allows us to
+ * manually conduct admin user access uses a simple script.
  */
 const ADMIN_TOKEN = getenv(
   "DANGEROUSLY_WHITELISTED_PUBLIC_ADMIN_ACCESS_TOKEN",
@@ -29,7 +38,7 @@ export class AdminAuthGuard extends AuthGuard("jwt") {
       // Allow whitelisted users to pass:
       if (user) {
         const { email } = user.profile;
-        if (WHITELISTED_ADMIN_EMAILS.has(email)) {
+        if (ADMIN_EMAILS.has(email)) {
           return user;
         }
       }
@@ -38,9 +47,34 @@ export class AdminAuthGuard extends AuthGuard("jwt") {
       const requestHeaders = context.args[0].headers;
       const requestToken = requestHeaders.admin_access_token;
 
-      if (requestToken === ADMIN_TOKEN) {
-        // NOTE: No real user in this case!
-        return {};
+      if (Boolean(ADMIN_TOKEN) && requestToken === ADMIN_TOKEN) {
+        // Welcome to the world of Technical Debt! This is a fake user!
+        // We should not allow manual usage of the admin API like this
+        // but this is a temporary measure to get us off the ground without
+        // a proper admin UI. I do not think any admin APIs currently use
+        // the req.user object but it is possible they could try and it will
+        // not exist for the admin requests using the temporary access token
+        // so we create an obviously fake user profile here to return. All
+        // of this should be removed once the proper admin UI is created.
+        const FAKE_ADMIN_PROFILE: UserProfile = {
+          lastActiveChallengeId: "",
+          uuid: "admin_uuid",
+          email: "sean@pairwise.tech",
+          givenName: "ADMIN",
+          familyName: "ADMIN",
+          avatarUrl: "ADMIN",
+          displayName: "FIXED ADMIN USER",
+        };
+
+        const FAKE_ADMIN_USER = {
+          profile: FAKE_ADMIN_PROFILE,
+          payments: [],
+          settings: {},
+          courses: {},
+          progress: {},
+        };
+
+        return FAKE_ADMIN_USER;
       }
     } catch (err) {
       throw new BadRequestException();
