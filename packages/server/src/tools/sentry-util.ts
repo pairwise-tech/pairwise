1065aee6ae25356116fd46e89b0067e5b1465d4e
Sean Smith <sean.smith.2009@gmail.com>
2020-03-08 13:16:53 +0800

    Update sentry-util

--- packages/server/src/tools/sentry-util.ts
+++ packages/server/src/tools/sentry-util.ts
@@ -4,3 +4,10 @@ import * as Sentry from "@sentry/node";
 export const captureSentryException = (error: any) => {
   Sentry.captureException(error);
 };
+
+// Report a specific message to Sentry
+// This is probably not as useful as reporting an error directly but it
+// could but useful to report specific messages if we need to do that.
+export const captureSentryMessage = (message: string) => {
+  Sentry.captureMessage(message);
+};
