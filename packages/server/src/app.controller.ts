48cef87b579027cb8b00f75ce0abc52eab66f95e
Sean Smith <sean.smith.2009@gmail.com>
2020-03-08 12:01:54 +0800

    Remove temproray Sentry/error testing code

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -6,20 +6,6 @@ import * as Sentry from "@sentry/node";
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/error")
-  testError() {
-    try {
-      console.log("Sentry capture message");
-      Sentry.captureMessage("This is a message!");
-      throw new Error("Pairwise blew up!");
-    } catch (err) {
-      console.log("Sentry capture error");
-      Sentry.captureException(err);
-    }
-
-    return "Started fires! 🔥";
-  }
-
   @Get()
   getIndex(): string {
     return this.appService.getIndex();
