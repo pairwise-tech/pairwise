f03d00b24179c24c803fb8b33d39eb04d5c8d56c
Ian Sinnott <ian@iansinnott.com>
2020-03-13 10:15:59 +0800

    Update Torvalds to test for challenges without tests
    Any challenge with a workspace is assumed to have a test suite. This
    assumption could be relaxed at some later point but for now the
    reasoning is: If you have a workspace, you want to test some user code.
    If you are just instructing, then use a challenge type of
    media/section/project/etc where there is no assumption of test code.

--- packages/client/global.d.ts
+++ packages/client/global.d.ts
@@ -66,3 +66,7 @@ declare module "string-quote-x" {
   const quote: (str: string) => string;
   export default quote;
 }
+
+declare module "strip-comments" {
+  export default function stripComments(s: string): string;
+}
--- packages/client/src/test/Torvalds.test.ts
+++ packages/client/src/test/Torvalds.test.ts
@@ -8,6 +8,7 @@ import {
   TestCase,
 } from "tools/test-utils";
 import { wait } from "tools/utils";
+import stripComments from "strip-comments";
 
 /** ===========================================================================
  * Config & Utils
@@ -89,7 +90,7 @@ describe("Linus should be able to pass all the challenges first try", () => {
     let failedTests: string[] = [];
 
     /* Get all the challenges */
-    const challenges = course.modules.map(m => m.challenges).flat();
+    const challenges = course.modules.flatMap(m => m.challenges);
 
     /* For every challenge, execute the tests */
     outerLoop: for (const challenge of challenges) {
@@ -142,44 +143,57 @@ describe("Linus should be able to pass all the challenges first try", () => {
 
       let results: TestCase[] = [];
 
-      /* Load the document */
-      document.body.innerHTML = doc;
+      // The test code string might not be empty but still have no test code so we run this check.
+      const hasTestCode = stripComments(challenge.testCode).trim();
 
-      /* Add the message listener */
-      window.parent.postMessage = (data: IframeMessageEvent["data"]) => {
-        const { source, message } = data;
-        if (source === "TEST_RESULTS") {
-          results = JSON.parse(message);
-        }
-      };
-
-      try {
-        handleAbsurdScriptEvaluation(script);
-      } catch (err) {
-        console.error("Error thrown from window.eval!", err);
-
-        /* The test failed, enter a failed test case manually */
-        const failedTestCase = { testResult: false };
-        results = [failedTestCase as TestCase];
-      }
-
-      /**
-       * Wait for the test script to execute and post a message back to
-       * the message listener.
-       */
-      try {
-        await waitForResults({ results });
-      } catch (err) {
-        // waitForResults can throw, it may if the polling timeout is
-        // exceeded. Catch the error here and handle it as a failed test.
-        results = [
+      if (!hasTestCode) {
+        results = results.concat([
           {
             testResult: false,
-            test: "Running waitForResults to retrieve test results",
-            message:
-              "waitForResults threw and error, probably because of a timeout",
+            test: "Zero test cases",
+            message: `No test cases found for challenge [${challenge.id}] ${challenge.title}. Maybe this should be a media challenge?`,
           },
-        ];
+        ]);
+      } else {
+        /* Load the document */
+        document.body.innerHTML = doc;
+
+        /* Add the message listener */
+        window.parent.postMessage = (data: IframeMessageEvent["data"]) => {
+          const { source, message } = data;
+          if (source === "TEST_RESULTS") {
+            results = JSON.parse(message);
+          }
+        };
+
+        try {
+          handleAbsurdScriptEvaluation(script);
+        } catch (err) {
+          console.error("Error thrown from window.eval!", err);
+
+          /* The test failed, enter a failed test case manually */
+          const failedTestCase = { testResult: false };
+          results = [failedTestCase as TestCase];
+        }
+
+        /**
+         * Wait for the test script to execute and post a message back to
+         * the message listener.
+         */
+        try {
+          await waitForResults({ results });
+        } catch (err) {
+          // waitForResults can throw, it may if the polling timeout is
+          // exceeded. Catch the error here and handle it as a failed test.
+          results = [
+            {
+              testResult: false,
+              test: "Running waitForResults to retrieve test results",
+              message:
+                "waitForResults threw and error, probably because of a timeout",
+            },
+          ];
+        }
       }
 
       /**
