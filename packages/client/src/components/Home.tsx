0a8cab41b54779d7ed2762f44f00f78fc2f2617c
Sean Smith <sean.smith.2009@gmail.com>
2020-03-29 21:54:39 +0800

    Cleanup changes

--- packages/client/src/components/Home.tsx
+++ packages/client/src/components/Home.tsx
@@ -22,7 +22,6 @@ interface IState {}
 
 class Home extends React.Component<IProps, IState> {
   render(): Nullable<JSX.Element> {
-    console.log(this.props.skeletons);
     return (
       <PageContainer>
         <ContentContainer>
