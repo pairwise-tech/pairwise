f69af2e231fc09fc4bb1fc2e353f81acda734585
Sean Smith <sean.smith.2009@gmail.com>
2020-04-06 15:26:35 +0800

    Add Shortest String challenge

--- packages/client/src/tools/test-utils.ts
+++ packages/client/src/tools/test-utils.ts
@@ -85,6 +85,7 @@ export const TEST_UTILS_GLOBALS = {
   pass: "A function to just pass a test",
   fail: "A function to just fail a test",
   stringList: "An array of 100 random strings",
+  loremIpsum: "A paragraph of Lorem Ipsum text",
 };
 
 // Type representing all the globally available key names
@@ -617,6 +618,8 @@ export const compileCodeString = async (
  * challenge tests.
  */
 const TEST_HELPER_CONSTANTS = `
+const loremIpsum = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.";
+
 const stringList = [
   "QVRYdrkdVw",
   "0pWPuoHqRw",
