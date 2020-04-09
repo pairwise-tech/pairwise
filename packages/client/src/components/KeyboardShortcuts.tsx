dc58edccdd6fbfd34be844374acde029bd5e1ff4
Sean Smith <sean.smith.2009@gmail.com>
2020-04-09 14:20:06 +0800

    Add keyboards shortcuts for GreatSuccess modal

--- packages/client/src/components/KeyboardShortcuts.tsx
+++ packages/client/src/components/KeyboardShortcuts.tsx
@@ -24,8 +24,8 @@ type ShortcutKeyHandler = (e: KeyboardEvent) => any;
 const SHORTCUT_KEYS = {
   "cmd+shift+k": "Navigate to Sandbox",
   "cmd+j": "Toggle Navigation Map",
-  "cmd+,": "Navigate to Previous Challenge",
-  "cmd+.": "Navigate to Next Challenge",
+  "opt+,": "Navigate to Previous Challenge",
+  "opt+.": "Navigate to Next Challenge",
   "cmd+;": "Toggle Full Screen Editor",
   "cmd+p": "Focus Search Bar",
 };
