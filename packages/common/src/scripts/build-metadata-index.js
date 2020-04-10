19f1129fdf923e6a064fb92705e479115d1bae6b
Ian Sinnott <ian@iansinnott.com>
2020-04-11 01:45:28 +0800

    Refactor metadata build script. Ignore unchanged builds

--- packages/common/src/scripts/build-metadata-index.js
+++ packages/common/src/scripts/build-metadata-index.js
@@ -1,7 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+
 const {
   getChallengMetadata,
   readCourseFilesFromDisk,
 } = require("./get-challenge-metadata");
+
+const log = (...args) => {
+  console.log("[COURSE METADATA]", ...args);
+};
 
 const hasTodo = challenge => {
   return Object.values(challenge)
@@ -10,7 +18,47 @@ const hasTodo = challenge => {
 };
 
 const main = () => {
-  const courseFiles = readCourseFilesFromDisk();
+  const outfile = path.resolve(__dirname, "../courses/metadata.json");
+  const courseDir = path.resolve(__dirname, "../courses");
+
+  let existingFile;
+  try {
+    existingFile = JSON.parse(fs.readFileSync(outfile, { encoding: "utf-8" }));
+  } catch (err) {
+    console.error("No existig file found or parse failure.");
+  }
+
+  // TODO: We can use this to git diff the course dir against this commit. If
+  const currentCommit = execSync(`git log -n 1 --pretty=format:"%h"`, {
+    encoding: "utf-8",
+  }).trim();
+
+  if (existingFile) {
+    const { buildCommit } = existingFile["@@PAIRWISE"];
+    // const cmd = `git diff --name-only 6177d21 -- ${courseDir}`;
+    const cmd = `git diff --name-only ${buildCommit} -- ${courseDir}`;
+    const filesChanged = execSync(cmd, { encoding: "utf-8" }).trim();
+
+    if (!filesChanged) {
+      log(
+        `No course files changed since last build commit ${buildCommit}. Course metadata will not be rebuilt.`,
+      );
+      return;
+    }
+
+    log("The following course files have changed:");
+    log(
+      filesChanged
+        .split("\n")
+        .map(x => `- ${x}`)
+        .join("\n"),
+    );
+    log("Index will be rebuilt.");
+  }
+
+  log("Building index...");
+
+  const courseFiles = readCourseFilesFromDisk(courseDir);
 
   const allChallenges = courseFiles
     .map(x => x.json)
@@ -19,6 +67,7 @@ const main = () => {
 
   const result = {
     "@@PAIRWISE": {
+      buildCommit: currentCommit,
       totalChallenges: allChallenges.length,
       codeChallenges: allChallenges.filter(x => x.solutionCode).length,
       videoChallenges: allChallenges.filter(x => x.videoUrl).length,
@@ -35,8 +84,11 @@ const main = () => {
     }
   });
 
-  // Pipe the output to some file
-  console.log(JSON.stringify(result, null, 2));
+  log(`Writing to ${outfile}`);
+  fs.writeFileSync(outfile, JSON.stringify(result, null, 2), {
+    encoding: "utf-8",
+  });
+  log("Build Successful.");
 };
 
 main();
