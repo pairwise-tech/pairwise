d54ee4cd8b77603a26d06c53fc9740de76e88ca2
Ian Sinnott <ian@iansinnott.com>
2020-04-11 21:55:10 +0800

    Add SEO component to workspace

---
+++ packages/client/src/components/SEO.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Helmet from "react-helmet";
+
+type MetaProps = JSX.IntrinsicElements["meta"];
+
+interface SEOProps {
+  title: string;
+  description: string;
+  lang?: string;
+}
+
+/**
+ * SEO component copied largely from our WWW package. That version is using
+ * gatsby queries to get some of the data though so it's not compatible with our
+ * workspace.
+ */
+const SEO = ({ title, description, lang = "en" }: SEOProps) => {
+  const metaTags: MetaProps[] = [
+    {
+      name: "description",
+      content: description,
+    },
+    {
+      name: "twitter:card",
+      content: "summary",
+    },
+    {
+      name: "twitter:title",
+      content: title,
+    },
+    {
+      name: "twitter:description",
+      content: description,
+    },
+    {
+      property: "og:title",
+      content: title,
+    },
+    {
+      property: "og:description",
+      content: description,
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+  ];
+
+  return (
+    <Helmet
+      htmlAttributes={{ lang }}
+      title={title}
+      titleTemplate={`%s | Pairwise Workspace`}
+    >
+      {metaTags.map(meta => {
+        return <meta key={Object.values(meta).join("-")} {...meta} />;
+      })}
+    </Helmet>
+  );
+};
+
+export default SEO;
