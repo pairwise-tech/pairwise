888c62638aa29f1e0f963921eccb9f35d0be56fd
Ian Sinnott <ian@iansinnott.com>
2020-06-06 15:42:04 +0800

    Add just work script

---
+++ just-work.sh
@@ -0,0 +1,20 @@
+# This is meant to Just Work™ when you're running into build related issues and
+# really just want things to run so you can get back to work.
+
+echo '[DO] Make sure yarn dependencies are up to date'
+yarn install
+
+echo '[DO] Stop running docker containers'
+docker-compose stop
+
+echo '[DO] Stop any remaining containers via direct docker command'
+docker ps -q | xargs docker stop
+
+echo '[DO] Rebuld all the docker stuff'
+yarn run docker:dependencies
+yarn run docker:build
+
+echo '[DO] Start up all our docker stuff again. Run it in the background'
+yarn run up:build -d
+
+echo "[DONE] Everything rebuilt. If nothing failed yet you should be good to go."
