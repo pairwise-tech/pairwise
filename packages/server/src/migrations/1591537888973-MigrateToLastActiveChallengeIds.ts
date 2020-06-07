d7102c3031ce654db3da16aa9a4da970f8e5f156
Sean Smith <sean.smith.2009@gmail.com>
2020-06-07 23:28:26 +0800

    Format migration file

--- packages/server/src/migrations/1591537888973-MigrateToLastActiveChallengeIds.ts
+++ packages/server/src/migrations/1591537888973-MigrateToLastActiveChallengeIds.ts
@@ -1,18 +1,30 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class MigrateToLastActiveChallengeIds1591537888973 implements MigrationInterface {
-    name = 'MigrateToLastActiveChallengeIds1591537888973'
+export class MigrateToLastActiveChallengeIds1591537888973
+  implements MigrationInterface {
+  name = "MigrateToLastActiveChallengeIds1591537888973";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "lastActiveChallengeId" TO "lastActiveChallengeIds"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastActiveChallengeIds"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "lastActiveChallengeIds" jsonb NOT NULL DEFAULT '"{}"'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" RENAME COLUMN "lastActiveChallengeId" TO "lastActiveChallengeIds"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP COLUMN "lastActiveChallengeIds"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "lastActiveChallengeIds" jsonb NOT NULL DEFAULT '"{}"'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastActiveChallengeIds"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "lastActiveChallengeIds" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "lastActiveChallengeIds" TO "lastActiveChallengeId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP COLUMN "lastActiveChallengeIds"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "lastActiveChallengeIds" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" RENAME COLUMN "lastActiveChallengeIds" TO "lastActiveChallengeId"`,
+    );
+  }
 }
