4ab96feb275dd026ae5ac716e16eede061760a91
Sean Smith <sean.smith.2009@gmail.com>
2020-06-08 13:28:51 +0800

    Remove console statement

--- packages/client/src/components/Home.tsx
+++ packages/client/src/components/Home.tsx
@@ -68,7 +68,6 @@ class Home extends React.Component<IProps, IState> {
     // Determine the course challenge to link to
     let lastActiveChallengeExists = false;
     let courseChallengeLinkId = firstCourseChallenge.id;
-    console.log(lastActiveChallengeIds);
     if (courseId in lastActiveChallengeIds) {
       lastActiveChallengeExists = true;
       courseChallengeLinkId = lastActiveChallengeIds[courseId];
