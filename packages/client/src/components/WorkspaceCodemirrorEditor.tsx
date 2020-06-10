00a66d13c7119c3a5564f0034c4abeef933ccf51
Ian Sinnott <ian@iansinnott.com>
2020-06-10 23:51:16 +0800

    No more redirecting to a page telling how poor our mobile UX is!

--- packages/client/src/components/WorkspaceCodemirrorEditor.tsx
+++ packages/client/src/components/WorkspaceCodemirrorEditor.tsx
@@ -106,7 +106,6 @@ export default class WorkspaceCodemirrorEditor
       mode,
       theme: "material-darker",
       lineNumbers: true,
-      cursorScrollMargin: 60,
       matchBrackets: true,
       matchTags: true,
       autoCloseBrackets: true,
