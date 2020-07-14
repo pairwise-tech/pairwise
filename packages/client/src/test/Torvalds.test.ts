56496278d891cdb3fc9d4e9d42c07d7cdadb9dc2
Sean Smith <sean.smith.2009@gmail.com>
2020-07-14 15:39:50 +0800

    Skip Async Practice tests in Torvalds unit test

--- packages/client/src/test/Torvalds.test.ts
+++ packages/client/src/test/Torvalds.test.ts
@@ -58,16 +58,17 @@ const DANGEROUSLY_SKIP_CHALLENGE = new Set([
   "Ao8hbaiP", // Test env seems to be having trouble calculating the midpoint of a bounding box.
   // "pUf7$Qi2y", // Could not test that p tags are white... WHY? See NOTE
   // "MEjox@iw0",
-  // NOTE: The next 5 ids are the Async Module Practice with APIs challenges, which all
-  // call external service APIs. These are disabled in the test environment. These
-  // were tested after creation to ensure they worked, and then disabled. A better
-  // solution might be to somehow mock these external APIs (?).
+
+  // NOTE: The next 5 ids are the Async Module Practice with APIs challenges,
+  // which all call external service APIs. These are disabled in the test environment.
+  // These were tested after creation to ensure they worked, and then disabled.
+  // A better solution might be to somehow mock these external APIs (?).
   // **********
-  // "CgstSAbnS",
-  // "yYHjlEO$4",
-  // "hbAfMbUAT0",
-  // "RBkHLlPHT",
-  // "aC5pqM5B6",
+  "CgstSAbnS",
+  "yYHjlEO$4",
+  "hbAfMbUAT0",
+  "RBkHLlPHT",
+  "aC5pqM5B6",
   // **********
 ]);
 
