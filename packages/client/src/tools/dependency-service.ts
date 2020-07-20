61956b086dea1ef0f7ec46531519eb3fc857d997
Sean Smith <sean.smith.2009@gmail.com>
2020-07-20 23:13:27 +0800

    Update react-native-web tests

--- packages/client/src/tools/dependency-service.ts
+++ packages/client/src/tools/dependency-service.ts
@@ -1,29 +1,6 @@
 import axios from "axios";
 
 /** ===========================================================================
- * External Libraries
- * ============================================================================
- */
-
-const IS_TEST = process.env.NODE_ENV === "test";
-
-let ReactNativeWebSourceUrl = "";
-
-/**
- * Not very ideal, but Jest has some issues with the file-loader! Fine!
- */
-if (IS_TEST) {
-  const fs = require("fs");
-  ReactNativeWebSourceUrl = fs.readFileSync("src/js/react-native-web-lib.js", {
-    encoding: "utf8",
-  });
-} else {
-  // @ts-ignore
-  // eslint-disable-next-line import/no-webpack-loader-syntax
-  ReactNativeWebSourceUrl = require("file-loader!../js/react-native-web-lib.js");
-}
-
-/** ===========================================================================
  * Types & Config
  * ============================================================================
  */
@@ -36,7 +13,34 @@ interface Dependency {
 type DependencyCache = Map<string, Dependency>;
 
 interface SourceLibraryMap {
-  [key: string]: string;
+  [key: string]: Source;
+}
+
+type SourceFn = () => string;
+type Source = string | SourceFn;
+
+const IS_TEST = process.env.NODE_ENV === "test";
+
+/** ===========================================================================
+ * Handle External Libraries
+ * ============================================================================
+ */
+
+let ReactNativeWebSourceUrl: Source = "";
+
+/**
+ * Not very ideal, but Jest has some issues with the file-loader! Fine!
+ */
+if (IS_TEST) {
+  const fs = require("fs");
+  const lib = fs.readFileSync("src/js/react-native-web-lib.js", {
+    encoding: "utf8",
+  });
+  ReactNativeWebSourceUrl = () => lib;
+} else {
+  // @ts-ignore
+  // eslint-disable-next-line import/no-webpack-loader-syntax
+  ReactNativeWebSourceUrl = require("file-loader!../js/react-native-web-lib.js");
 }
 
 /**
@@ -62,63 +66,62 @@ const SOURCE_LIBRARY_MAP: SourceLibraryMap = {
  */
 
 class DependencyCacheClass {
-  dependencies: DependencyCache = new Map();
+  dependencyCache: DependencyCache = new Map();
   sourceLibraries = new Map();
 
   constructor(sourceLibraryMap: SourceLibraryMap) {
     this.sourceLibraries = new Map(Object.entries(sourceLibraryMap));
   }
 
-  getDependency = async (packageName: string) => {
-    if (this.dependencies.has(packageName)) {
+  getDependency = async (packageName: string): Promise<string> => {
+    if (this.dependencyCache.has(packageName)) {
       /**
        * The package is cached just return the code.
        */
-      const dependency = this.dependencies.get(packageName) as Dependency;
+      const dependency = this.dependencyCache.get(packageName) as Dependency;
       return dependency.source;
     } else {
       if (this.sourceLibraries.has(packageName)) {
         try {
           // Get the source library:
-          const uri = this.sourceLibraries.get(packageName) as string;
-          const source = await this.fetchResource(uri);
-          this.dependencies.set(packageName, { source });
-          return source;
+          const resolver = this.sourceLibraries.get(packageName);
+          let lib: string = "";
+
+          if (typeof resolver === "function") {
+            lib = resolver();
+          } else {
+            lib = await this.fetchResource(resolver);
+          }
+
+          this.dependencyCache.set(packageName, { source: lib });
+          return lib;
         } catch (err) {
           const msg = `[ERROR]: Failed to fetch source for ${packageName}. Error: ${err.message}`;
           console.log(msg);
           throw new Error(msg);
         }
+      } else {
+        console.warn(`No source library exists for package: ${packageName}`);
+        return "";
       }
     }
   };
 
   fetchResource = async (uri: string) => {
-    try {
-      /**
-       * If it's not a URL we are in test mode and it's the actual source
-       * file... this will throw and the source is returned. Yolo!
-       */
-      // @ts-ignore
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const url = new URL(uri);
-      /**
-       * NOTE: Sometimes these requests to unpkg can fail!
-       *
-       * We should review this later and try to make this more robust.
-       * Currently, the Cypress test for React challenges is disabled
-       * for this very reason.
-       */
-      const response = await axios.get(uri, {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-      return response.data;
-    } catch (err) {
-      return uri;
-    }
+    /**
+     * NOTE: Sometimes these requests to unpkg can fail!
+     *
+     * We should review this later and try to make this more robust.
+     * Currently, the Cypress test for React challenges is disabled
+     * for this very reason.
+     */
+    const response = await axios.get(uri, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    return response.data;
   };
 }
 
