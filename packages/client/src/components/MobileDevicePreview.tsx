4953a2d309aa73c7f7d5ef095cdd1cc0314e94b9
Sean Smith <sean.smith.2009@gmail.com>
2020-08-23 13:43:22 +0800

    Add Galaxy Note 8 preview HTML

--- packages/client/src/components/MobileDevicePreview.tsx
+++ packages/client/src/components/MobileDevicePreview.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+/** ===========================================================================
+ * HTML for Mobile Device Previews
+ * ----------------------------------------------------------------------------
+ * See https://marvelapp.github.io/devices.css/ for details.
+ * ============================================================================
+ */
+
 export const IPhoneXMobilePreview = (props: { children: JSX.Element }) => {
   return (
     <div className="marvel-device iphone8plus black">
@@ -12,6 +19,24 @@ export const IPhoneXMobilePreview = (props: { children: JSX.Element }) => {
       <div className="screen">{props.children}</div>
       <div className="home"></div>
       <div className="bottom-bar"></div>
+    </div>
+  );
+};
+
+export const GalaxyNotePreview = (props: { children: JSX.Element }) => {
+  return (
+    <div className="marvel-device note8">
+      <div className="inner"></div>
+      <div className="overflow">
+        <div className="shadow"></div>
+      </div>
+      <div className="speaker"></div>
+      <div className="sensors"></div>
+      <div className="more-sensors"></div>
+      <div className="sleep"></div>
+      <div className="volume"></div>
+      <div className="camera"></div>
+      <div className="screen">{props.children}</div>
     </div>
   );
 };
