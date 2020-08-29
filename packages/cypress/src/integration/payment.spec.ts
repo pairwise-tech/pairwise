99de08c39e14312266debce8ae32d1cc0b0e4b93
Sean Smith <sean.smith.2009@gmail.com>
2020-08-29 12:51:06 +0800

    Fix payment.spec test

--- packages/cypress/src/integration/payment.spec.ts
+++ packages/cypress/src/integration/payment.spec.ts
@@ -52,7 +52,7 @@ describe("Payment Course Flow: A user can purchase a course and unlock it's cont
     // NOTE: This relies on fixed text and the existing challenge list,
     // it will need to be updated if/when these challenges change (which
     // they will).
-    cy.contains("It's time to start using some real databases!");
+    cy.contains("Getting started with SQL");
     // Paid content label should not exist anymore
     cy.get("#paid-content-label").should("not.exist");
     click("workspace-next-challenge-button");
