4ba7a3fd0bccc2dc8959b54c1d37d6806bf17f97
Ian Sinnott <ian@iansinnott.com>
2020-09-18 00:52:36 +0800

    Add a bit of user routing

--- packages/admin-client/src/app/core.cljs
+++ packages/admin-client/src/app/core.cljs
@@ -1,22 +1,28 @@
 (ns app.core
   "This namespace contains your application and is the entrypoint for 'yarn start'."
+  (:require-macros [clojure.core.async :refer [go go-loop]])
   (:require [reagent.core :as r]
+            [clojure.core.async :as async :refer [>! <! chan]]
             [clojure.string :refer [starts-with?]]
             [clojure.spec.alpha :as spec]
             [clojure.spec.gen.alpha :as gen]
             [clojure.test.check.generators]
+            [ajax.core :as ajax]
             ["@blueprintjs/core" :as bp]
             [app.hello :refer [hello]]))
 
 ;; Generate emails. They are all at gmamil.com, but i'm not sure how to combine
 ;; multiple string generators yet
-(def string-alnum-gen (gen/such-that #(and (not= % "") (> (count %) 5)) (gen/string-alphanumeric)))
+(def string-alnum-gen (gen/such-that #(and (not= % "") (> (count %) 5)) (gen/string-alphanumeric) 100))
 (def email-host-gen (spec/gen #{"gmail.com" "hotmail.com" "yahoo.com.tw" "protonmail.com" "fastmail.io"}))
 
 ;; NOTE Spec or requires a keyword along with the predicate to help identify
 ;; which test failed. Not sure why spec/and doesn't require this though
 ;; (spec/def ::email (spec/or :string string? :nil nil?))
 ;; NOTE Use the nilable helper rather than or if you just want nilable values
+
+
+(spec/def ::guid uuid?)
 (spec/def ::email
   (spec/with-gen
     (spec/and string?
@@ -24,15 +30,24 @@
     #(gen/fmap (fn [[a b]] (str a "@" b))
                (gen/tuple string-alnum-gen email-host-gen))))
 
+(clj->js {:wee true})
+(def q #queue [:a :b :c])
+(def v [:a :b :c])
+(println {:hey "you"})
+(println #js{:hey "you"})
+(js/console.log {:hey "you"})
+(js/console.log #js{:hey "you"})
+
 (spec/def ::name (spec/nilable string?))
-(spec/def ::user (spec/keys :req-un [::email ::name]))
+(spec/def ::user (spec/keys :req-un [::guid ::email ::name]))
 
 (comment
   (gen/sample (gen/tuple string-alnum-gen email-host-gen))
   (spec/valid? string? "")
   (spec/valid? nil? "hey")
 
-  (spec/valid? ::user {:name "Ian" :email "mail@mail.com"})
+  ;; Hm, not actually sure what a uuid is
+  (spec/valid? ::user {:guid (uuid "3f716909-e631-4458-b820-6a46cf5113b0") :name "Ian" :email "mail@mail.com"})
 
   (spec/valid? ::email "hey@mail.com")
   (spec/valid? ::email "hey")
@@ -68,9 +83,16 @@
   (when (not= (:route prev-state) (:route next-state))
     (js/history.pushState {} "" (:route next-state))))
 
+;; FIXME This doesn't work for some reason. Breaks the back button, but
+;; seemingly not consistently
+(defn sync-url-to-state! [_e]
+  (let [pathname (-> js/document (.-location) (.-href) href->pathname)]
+    (swap! state update :route (constantly pathname))))
+
 (defn app-init
   "Initialize the App. Run one-time setup."
   []
+  (js/addEventListener "popstate" sync-url-to-state!)
   (println "Initializing APP with state", @state)
   (-> bp/FocusStyleManager .onlyShowFocusOnTabs)
   (swap! state update :route (constantly (href->pathname js/location.href))) ;; Set initial route from URL
@@ -108,6 +130,46 @@
                   :class (if (active? (:href props)) "active")}
                  (omit-keys props [:exact])) children])))
 
+(let [a (for [user (gen/sample (spec/gen ::user))]
+          (:guid user))
+      b (map :guid (gen/sample (spec/gen ::user)))]
+  (println (= a b)))
+
+(defn fetch-users
+  []
+  (for [user (gen/sample (spec/gen ::user))] user))
+
+(comment (fetch-users))
+
+(defn Users
+  ([] [Users []])
+  ([users]
+   (if (empty? users)
+     [:div.users "No users :("]
+     [:div.users
+      [:h1 "Users"]
+      [:p "A list of users can go here"]
+      [:> bp/Button {:on-click #(println "should fetch")} "Fetch Users"]
+      [:div.user-list
+       (for [u users]
+         [:div
+          {:key (:guid u)}
+          [:p [:strong "guid:"] " " (-> u :guid str)]
+          [:p [:string "name:"] " " (or (:name u) "<Unknown>")]
+          [:p [:string "email:"] " " (:email u)]
+          [Link {:href (str "users/" (:guid u))} "View"]])]])))
+
+(defn route->guid [route] (second (re-matches #"/users/([\w-]+)" route)))
+
+(defn UserDetails
+  []
+  (let [guid (route->guid (:route @state))]
+    (if-not guid
+      [:div [:h1 "User Not Found"] [:p "No user found for guid."]]
+      [:div
+       [:h1 "User Details"]
+       [:p [:string "guid: "] guid]])))
+
 (defn Home
   []
   [:div.home
@@ -130,7 +192,7 @@
 
 (defn Header []
   [:nav.flex.Header
-   [Link {:href "/" :exact true} "Home"]
+   [Link {:href "/" :exact true} "Users"]
    [Link {:href "/about"} "About"]
    [Link {:href "/hello"} "Hello"]
    [:div.right
@@ -139,18 +201,21 @@
 
 (defn App
   []
-  (app-init)
-  (fn []
-    [:div.main
-     {:class "merge-class"} ;; Just showing myself that classes will be merged
-     [Header]
-     [:hr]
-     [:div.routed
-      (case (:route @state)
-        "/" [Home]
-        "/about" [About]
-        "/hello" [hello]
-        [NotFound])]]))
+  (let [_ (app-init)
+        users (fetch-users)]
+    (fn []
+      [:div.main
+       {:class "merge-class"} ;; Just showing myself that classes will be merged
+       [Header]
+       [:hr]
+       [:div.routed
+        (let [route (:route @state)]
+          (cond
+            (= route "/") [Users users]
+            (route->guid route) [UserDetails]
+            (= route "/about") [About]
+            (= route "/hello") [hello]
+            :else [NotFound]))]])))
 
 (defn ^:dev/after-load render
   "Render the toplevel component for this app."
