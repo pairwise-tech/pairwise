fa8aa23c2daf109d1d210dfa94e2513becc6b39e
Sean Smith <sean.smith.2009@gmail.com>
2020-11-21 10:53:19 +0800

    Add additional logging steps in Stripe checkout flow

--- packages/server/src/payments/payments.service.ts
+++ packages/server/src/payments/payments.service.ts
@@ -86,6 +86,10 @@ export class PaymentsService {
     requestUser: RequestUser,
     courseId: string,
   ) {
+    console.log(
+      `[STRIPE]: Running handleCreatePaymentIntent for user: ${requestUser.profile.uuid} and courseId: ${courseId}`,
+    );
+
     const courseMetadata = ContentUtility.getCourseMetadata(courseId);
     if (!courseMetadata) {
       const err = new BadRequestException(
@@ -128,6 +132,7 @@ export class PaymentsService {
     signature: string,
   ) {
     try {
+      console.log(`[STRIPE]: Running handleStripeCheckoutSuccessWebhook`);
       // You must use the raw body:
       // @ts-ignore - the raw body is added by custom code in main.ts
       const { rawBody } = request;
