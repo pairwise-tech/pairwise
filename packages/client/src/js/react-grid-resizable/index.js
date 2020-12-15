de55d48f708ed8bccde0a9789376ed56b453ee1a
Sean Smith <sean.smith.2009@gmail.com>
2020-12-16 01:15:50 +0800

    Add comment explaining forked library

--- packages/client/src/js/react-grid-resizable/index.js
+++ packages/client/src/js/react-grid-resizable/index.js
@@ -1,5 +1,20 @@
 /* eslint-disable */
 "use strict";
+
+/**
+ * NOTE: This is a clone of the react-grid-resizable library, original
+ * source: https://github.com/netcell/react-grid-resizable.
+ *
+ * The library has not been updated and uses a React version which is
+ * incompatible with our local, upgraded React version. The library has a
+ * stale PR which is trying to fix this problem, which has not been merged.
+ * As a workaround, I cloned the library, ran 'npm build' and dropped
+ * the 'lib' output here and then imported these components in the
+ * Workspace. This could be changed in the future if the original
+ * library is upgraded, in which case the library could be re-installed
+ * and this code removed. For now, this works.
+ */
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const RowsWrapper_1 = require("./GridWrapper/RowsWrapper");
 exports.RowsWrapper = RowsWrapper_1.RowsWrapper;
