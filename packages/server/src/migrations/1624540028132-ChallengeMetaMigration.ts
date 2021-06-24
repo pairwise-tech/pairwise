7ea73d057ce0a969f35807db9a4fdd794ec77c1c
Sean Smith <sean.smith.2009@gmail.com>
2021-06-25 01:33:48 +0800

    Format challenge-meta migration file

--- packages/server/src/migrations/1624540028132-ChallengeMetaMigration.ts
+++ packages/server/src/migrations/1624540028132-ChallengeMetaMigration.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class ChallengeMetaMigration1624540028132 implements MigrationInterface {
-    name = 'ChallengeMetaMigration1624540028132'
+  name = "ChallengeMetaMigration1624540028132";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "challenge_meta" ("uuid" uuid NOT NULL DEFAULT uuid_generate_v4(), "challengeId" character varying NOT NULL, "numberOfTimesCompleted" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_6358562864ec5b146a438ae28a5" PRIMARY KEY ("uuid"))`);
-        await queryRunner.query(`CREATE INDEX "IDX_c57d21ad292aea7c64648cec87" ON "challenge_meta" ("challengeId") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "challenge_meta" ("uuid" uuid NOT NULL DEFAULT uuid_generate_v4(), "challengeId" character varying NOT NULL, "numberOfTimesCompleted" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_6358562864ec5b146a438ae28a5" PRIMARY KEY ("uuid"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_c57d21ad292aea7c64648cec87" ON "challenge_meta" ("challengeId") `,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "IDX_c57d21ad292aea7c64648cec87"`);
-        await queryRunner.query(`DROP TABLE "challenge_meta"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "IDX_c57d21ad292aea7c64648cec87"`);
+    await queryRunner.query(`DROP TABLE "challenge_meta"`);
+  }
 }
