b8408eab9dce2989b771a91c3f20e3cabff32802
Sean Smith <sean.smith.2009@gmail.com>
2021-06-24 15:59:37 +0800

    Update admin pull request view to display diffs for all courses

--- packages/server/src/admin/admin.controller.ts
+++ packages/server/src/admin/admin.controller.ts
@@ -263,103 +263,114 @@ export class AdminController {
       // Fetch the pull request diff
       const diff = await this.fetchPullRequestDiff(params.pull);
 
-      const matchFile = (fileName: string) => (item: any) => {
-        return item.find(x => x.filename === fileName);
+      const matchFile = (fileName: string) => (diff: any) => {
+        return diff.filename === fileName;
+      };
+
+      const filename = (name: string) => {
+        return `packages/common/src/courses/${name}.json`;
       };
 
       // Get all the course files
-      const dir = "packages/common/src/courses";
-      const ts = diff.find(matchFile(`${dir}/01_fullstack_typescript.json`));
-      const python = diff.find(matchFile(`${dir}/02_python_language.json`));
-      const rust = diff.find(matchFile(`${dir}/03_rust_language.json`));
-      const go = diff.find(matchFile(`${dir}/04_golang_language.json`));
-
+      const ts = diff.find(matchFile(filename("01_fullstack_typescript")));
+      const python = diff.find(matchFile("02_python_language"));
+      const rust = diff.find(matchFile("03_rust_language"));
+      const go = diff.find(matchFile("04_golang_language"));
       const courses = [ts, python, rust, go];
 
       if (courses.length > 0) {
-        return courses.map(async courseDiffFile => {
-          const { sha, patch } = courseDiffFile;
-          /**
-           * Extract all the git file annotations which denote changed line
-           * numbers in the diff.
-           */
-          const lineDiffs = patch
-            .split("\n")
-            .filter(x => /@@(.*)@@/.test(x))
-            .map(x => x.match(/\+(.*)\,/).pop());
+        const result = await Promise.all(
+          courses.map(async courseDiffFile => {
+            const { sha, patch } = courseDiffFile;
+            /**
+             * Extract all the git file annotations which denote changed line
+             * numbers in the diff.
+             */
+            const lineDiffs = patch
+              .split("\n")
+              .filter(x => /@@(.*)@@/.test(x))
+              .map(x => x.match(/\+(.*)\,/).pop());
 
-          /**
-           * Fetch the blob for the course JSON in file in this PR. Convert
-           * it to formatted JSON and split it by line so we can iterate
-           * through it with reference to the line numbers.
-           */
-          const blob = await this.fetchFileBlob(sha);
-          const blobJSON = JSON.stringify(blob, null, 2);
-          const jsonByLines = blobJSON.split("\n");
+            /**
+             * Fetch the blob for the course JSON in file in this PR. Convert
+             * it to formatted JSON and split it by line so we can iterate
+             * through it with reference to the line numbers.
+             */
+            const blob = await this.fetchFileBlob(sha);
+            const blobJSON = JSON.stringify(blob, null, 2);
+            const jsonByLines = blobJSON.split("\n");
 
-          /**
-           * Iterate through the JSON by line number and extract all the
-           * challenge ids which overlap with line numbers from the diff.
-           */
-          let currentChallengeId = null;
-          const challengeIds = [];
-          const lineNumberSet = new Set(lineDiffs.map(line => +line));
+            /**
+             * Iterate through the JSON by line number and extract all the
+             * challenge ids which overlap with line numbers from the diff.
+             */
+            let currentChallengeId = null;
+            const challengeIds = [];
+            const lineNumberSet = new Set(lineDiffs.map(line => +line));
 
-          for (let i = 1; i < jsonByLines.length + 1; i++) {
-            const lineNumber = i;
-            const line = jsonByLines[lineNumber - 1];
-            if (line.includes(`"id":`)) {
-              const id = line.match(/\"id\": \"(.*)\"/).pop();
-              currentChallengeId = id;
+            for (let i = 1; i < jsonByLines.length + 1; i++) {
+              const lineNumber = i;
+              const line = jsonByLines[lineNumber - 1];
+              if (line.includes(`"id":`)) {
+                const id = line.match(/\"id\": \"(.*)\"/).pop();
+                currentChallengeId = id;
+              }
+
+              if (lineNumberSet.has(lineNumber)) {
+                challengeIds.push(currentChallengeId);
+              }
             }
 
-            if (lineNumberSet.has(lineNumber)) {
-              challengeIds.push(currentChallengeId);
-            }
-          }
+            /**
+             * Lookup up the original challenge (if it exists) and the updated
+             * challenge.
+             */
+            const courses = this.contentService.fetchAllCoursesForAdmin();
+            const originalChallengeMap = createInverseChallengeMapping(courses);
+            const pullRequestChallengeMap = createInverseChallengeMapping([
+              blob,
+            ]);
 
-          /**
-           * Lookup up the original challenge (if it exists) and the updated
-           * challenge.
-           */
-          const courses = this.contentService.fetchAllCoursesForAdmin();
-          const originalChallengeMap = createInverseChallengeMapping(courses);
-          const pullRequestChallengeMap = createInverseChallengeMapping([blob]);
+            /**
+             * Map over the identified altered challenge ids from the pull request
+             * and construct content context to return in the response.
+             */
+            const prDiffContext = challengeIds.map(id => {
+              // May be undefined if updated challenge is new:
+              const originalChallengeMeta = originalChallengeMap[id];
+              // May be undefined if updated challenge is a deletion:
+              const updatedChallengeMeta = pullRequestChallengeMap[id];
 
-          /**
-           * Map over the identified altered challenge ids from the pull request
-           * and construct content context to return in the response.
-           */
-          const prDiffContext = challengeIds.map(id => {
-            // May be undefined if updated challenge is new:
-            const originalChallengeMeta = originalChallengeMap[id];
-            // May be undefined if updated challenge is a deletion:
-            const updatedChallengeMeta = pullRequestChallengeMap[id];
+              // One of them should exist...
+              const existing = originalChallengeMap
+                ? originalChallengeMap
+                : updatedChallengeMeta;
 
-            // One of them should exist...
-            const existing = originalChallengeMap
-              ? originalChallengeMap
-              : updatedChallengeMeta;
+              const { moduleId, courseId } = existing;
+              const originalChallenge = originalChallengeMeta
+                ? originalChallengeMeta.challenge
+                : null;
+              const updatedChallenge = updatedChallengeMeta
+                ? updatedChallengeMeta.challenge
+                : null;
 
-            const { moduleId, courseId } = existing;
-            const originalChallenge = originalChallengeMeta
-              ? originalChallengeMeta.challenge
-              : null;
-            const updatedChallenge = updatedChallengeMeta
-              ? updatedChallengeMeta.challenge
-              : null;
+              return {
+                id,
+                moduleId,
+                courseId,
+                updatedChallenge,
+                originalChallenge,
+              };
+            });
 
-            return {
-              id,
-              moduleId,
-              courseId,
-              updatedChallenge,
-              originalChallenge,
-            };
-          });
+            return prDiffContext;
+          }),
+        );
 
-          return prDiffContext;
-        });
+        // Flatten the results and remove null id diffs
+        return result
+          .reduce((flat, diff) => flat.concat(diff))
+          .filter(x => x.id !== null);
       } else {
         return "Course JSON has not been modified in this PR.";
       }
