241385ceebf8b0cf2b9643a873d151ce41eed5ce
Sean Smith <sean.smith.2009@gmail.com>
2021-09-11 12:41:46 -0600

    Fix test

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  public getIndex() {
+  public getIndex(): string {
     return this.appService.getIndex();
   }
 }
--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  public async getIndex() {
+  public getIndex(): string {
     return "This is Pairwise 😎";
   }
 }
