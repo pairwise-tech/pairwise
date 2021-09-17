042beaf8cbd43b0f11a3534ba7b6efd14aa5d553
Sean Smith <sean.smith.2009@gmail.com>
2021-09-16 20:58:02 -0500

    Refactor admin auth logic

--- packages/server/src/auth/admin.guard.ts
+++ packages/server/src/auth/admin.guard.ts
@@ -1,17 +1,24 @@
 import { AuthGuard } from "@nestjs/passport";
-import { UnauthorizedException, BadRequestException } from "@nestjs/common";
+import { UnauthorizedException } from "@nestjs/common";
 import getenv from "getenv";
 import { UserProfile } from "@pairwise/common";
 import { isAdminEmail } from "./admin-auth";
 
 /**
- * Hard-coded Admin Access Token. This is dangerous!!! This should be fully
- * removed in the future once a proper admin UI exists. This allows us to
- * manually conduct admin user access using a simple script.
+ * Hard-coded Admin Access Token. This is dangerous!!! Use with caution.
  */
 const ADMIN_TOKEN = getenv(
   "DANGEROUSLY_WHITELISTED_PUBLIC_ADMIN_ACCESS_TOKEN",
   "",
+);
+
+/**
+ * Admin access token authentication must be explicitly enabled
+ * at the deployment level.
+ */
+const ENABLE_ADMIN_ACCESS_TOKEN_AUTHENTICATION = getenv.bool(
+  "ENABLE_ADMIN_ACCESS_TOKEN_AUTHENTICATION",
+  false,
 );
 
 /**
@@ -24,56 +31,58 @@ export class AdminAuthGuard extends AuthGuard("jwt") {
   }
 
   public handleRequest(err, user, info, context) {
-    try {
-      // Allow whitelisted users to pass:
-      if (user) {
-        const { email } = user.profile;
-        if (isAdminEmail(email)) {
-          return user;
-        }
+    // Allow whitelisted users to pass:
+    if (user) {
+      if (isAdminEmail(user.profile.email)) {
+        return user;
+      } else {
+        throw new UnauthorizedException("Go away");
       }
-
-      // Allow whitelisted admin access tokens to pass:
-      const requestHeaders = context.args[0].headers;
-      const requestToken = requestHeaders.admin_access_token;
-
-      if (Boolean(ADMIN_TOKEN) && requestToken === ADMIN_TOKEN) {
-        // Welcome to the world of Technical Debt! This is a fake user!
-        // We should not allow manual usage of the admin API like this
-        // but this is a temporary measure to get us off the ground without
-        // a proper admin UI. I do not think any admin APIs currently use
-        // the req.user object but it is possible they could try and it will
-        // not exist for the admin requests using the temporary access token
-        // so we create an obviously fake user profile here to return. All
-        // of this should be removed once the proper admin UI is created.
-        const FAKE_ADMIN_PROFILE: UserProfile = {
-          uuid: "admin_uuid",
-          email: "admin-access-token-user@pairwise.tech",
-          emailVerified: false,
-          givenName: "ADMIN",
-          familyName: "ADMIN",
-          displayName: "FIXED ADMIN USER",
-          avatarUrl: "ADMIN",
-          githubAccountId: "admin-account-id",
-          facebookAccountId: "admin-account-id",
-          googleAccountId: "admin-account-id",
-          coachingSessions: 0,
-        };
-
-        const FAKE_ADMIN_USER = {
-          profile: FAKE_ADMIN_PROFILE,
-          payments: [],
-          settings: {},
-          courses: {},
-          progress: {},
-        };
-
-        return FAKE_ADMIN_USER;
-      }
-    } catch (err) {
-      throw new BadRequestException();
     }
 
-    throw new UnauthorizedException("Go away");
+    // Ensure the admin token authentication is allowed
+    if (!ENABLE_ADMIN_ACCESS_TOKEN_AUTHENTICATION) {
+      throw new UnauthorizedException("Go away");
+    }
+
+    // Allow whitelisted admin access tokens to pass:
+    const requestHeaders = context.args[0].headers;
+    const requestToken = requestHeaders.admin_access_token;
+
+    if (!!ADMIN_TOKEN && requestToken === ADMIN_TOKEN) {
+      return getFakeAdminUser();
+    } else {
+      throw new UnauthorizedException("Go away");
+    }
   }
 }
+
+/**
+ * Create a fake user profile for requests which rely on the
+ * hard-coded admin token for authentication.
+ */
+const getFakeAdminUser = () => {
+  const FAKE_ADMIN_PROFILE: UserProfile = {
+    uuid: "admin_uuid",
+    email: "admin-access-token-user@pairwise.tech",
+    emailVerified: false,
+    givenName: "ADMIN",
+    familyName: "ADMIN",
+    displayName: "FIXED ADMIN USER",
+    avatarUrl: "ADMIN",
+    githubAccountId: "admin-account-id",
+    facebookAccountId: "admin-account-id",
+    googleAccountId: "admin-account-id",
+    coachingSessions: 0,
+  };
+
+  const FAKE_ADMIN_USER = {
+    profile: FAKE_ADMIN_PROFILE,
+    payments: [],
+    settings: {},
+    courses: {},
+    progress: {},
+  };
+
+  return FAKE_ADMIN_USER;
+};
