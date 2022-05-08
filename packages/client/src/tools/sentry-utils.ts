6fd53b17f5f4f91d32fab463ea21b465862ebd63
Sean <sean.smith.2009@gmail.com>
2022-05-07 22:01:10 -0500

    Disable Sentry

--- packages/client/src/tools/sentry-utils.ts
+++ packages/client/src/tools/sentry-utils.ts
@@ -2,12 +2,14 @@ import * as Sentry from "@sentry/browser";
 
 // Report an exception to Sentry
 export const captureSentryException = (error: any) => {
-  Sentry.captureException(error);
+  // Note: Sentry is disabled.
+  // Sentry.captureException(error);
 };
 
 // Report a specific message to Sentry
 // This is probably not as useful as reporting an error directly but it
 // could but useful to report specific messages if we need to do that.
 export const captureSentryMessage = (message: string) => {
-  Sentry.captureMessage(message);
+  // Note: Sentry is disabled.
+  // Sentry.captureMessage(message);
 };
